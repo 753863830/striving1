694283a2fbb3c3cfc14851f38d6f2c64aa56cbaf
753863830 <31847356+753863830@users.noreply.github.com>
2017-09-13 00:20:16 +0800

    Add files via upload

---
+++ 未命名2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+cout<<"hello,gdfus!"<<endl;
+cout<<"©³©¥©¥©¥©¥©¥©¥©¥©¥©¥©¥©·"<<endl;
+cout<<"©§¨¨¨¨¨¨ 99.9%  ©§ "<<endl; 
+cout<<"©»©¥©¥©¥©¥©¥©¥©¥©¥©¥©¥©¿" <<endl;
+}
+
---
+++ 未命名3.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int a,b,c ;
+printf("第一个数是");
+cin>>a;
+printf("第二个数是");
+cin>>b;
+c=a+b;
+cout<<"两数之和为"<<c;
+return 0;
+}
+
+
